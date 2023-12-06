b438f921676977dc0516a1938cf1e35727142a4d
YutaNaist <yuta.yamamoto@ms.naist.jp>
2023-12-06 13:29:27 +0900

    commit from windows MSI

---
+++ python/python_init.bat
@@ -0,0 +1,7 @@
+@REM conda create -n main
+@REM conda activate main
+conda install -y -c conda-forge ipykernel yapf flake8 jupyter pyinstaller
+conda install -y -c conda-forge numpy matplotlib pandas scipy flask request BeautifulSoup4 opencv pydrive2 gspread oauth2client pyqt 
+ PyQt5 pyside6
+
+conda install -y -c conda-forge ipykernel yapf flake8 jupyter pyinstaller numpy matplotlib pandas scipy flask request BeautifulSoup4 opencv pydrive2 gspread oauth2client pyqt pyside6
