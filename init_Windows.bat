7b9e8f4620f8ad58b0ec1a9155ed19a0cb4431b6
YutaNaist <yuta.yamamoto@ms.naist.jp>
2024-08-08 19:33:04 +0900

    0.1.01 update init_windows

--- init_Windows.bat
+++ init_Windows.bat
@@ -6,4 +6,4 @@ mklink %LOCALAPPDATA%\nvim\init.lua %CD%\neovim_config\init.lua
 mkdir %HOMEPATH%\.vim
 mklink /D %HOMEPATH%\.vim\config %CD%\vim_config\config
 mklink %HOMEPATH%\_vimrc %CD%\vim_config\.vimrc
-mklink %HOMEPATH%\.vim\environment.vim %CD%\vim_config\environment_Window.vim
+mklink %HOMEPATH%\.vim\environment.vim %CD%\vim_config\environment_Windows.vim
