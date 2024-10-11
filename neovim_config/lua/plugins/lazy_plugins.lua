4cb802334d9214a6fa78973c9d0e215723568238
YutaNaist <yuta.yamamoto@ms.naist.jp>
2024-10-11 19:48:36 +0900

    Refactor lazy.nvim setup and plugin list
    - Refactored the lazy.nvim setup in the `lazy.lua` file to use a more concise and readable syntax.
    - Removed the `lazy_noVscode_plugins.lua` file as it is no longer needed.
    - Updated the plugin list in the `lazy_plugins.lua` file to include additional plugins and improve organization.

--- neovim_config/lua/plugins/lazy_plugins.lua
+++ neovim_config/lua/plugins/lazy_plugins.lua
@@ -1,49 +1,44 @@
-return {
-    {
-        'akinsho/toggleterm.nvim',
-        version = "*",
-        config = true
-      --{'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    },
-    {
-        'numToStr/Comment.nvim',
-        --opts = {
-            -- add any options here
-        --}
-    },
-    {
-        'vim-scripts/ReplaceWithRegister',
-    },
-    {
-        'vim-scripts/camelcasemotion',
-    },
-    {
-        'rhysd/clever-f.vim'
-    },
-    {
-        'wellle/targets.vim'
-    },
-    {
-        'monaqa/dial.nvim'
-    },
-    {
-        'haya14busa/vim-edgemotion'
-    },
-    {
-        'rapan931/lasterisk.nvim'
-    },
-    {
-        'kevinhwang91/nvim-hlslens'
-    },
-}
-
+local pluginlist = {}
+table.insert(pluginlist, {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true
+    -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+})
+table.insert(pluginlist, {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
+table.insert(pluginlist, {'numToStr/Comment.nvim' -- opts = {
+-- add any options here
+-- }
+})
+table.insert(pluginlist, {'vim-scripts/ReplaceWithRegister'})
+table.insert(pluginlist, {'vim-scripts/ReplaceWithRegister'})
+table.insert(pluginlist, {'vim-scripts/camelcasemotion'})
+table.insert(pluginlist, {'vim-scripts/camelcasemotion'})
+table.insert(pluginlist, {'rhysd/clever-f.vim'})
+table.insert(pluginlist, {'wellle/targets.vim'})
+table.insert(pluginlist, {'monaqa/dial.nvim'})
+table.insert(pluginlist, {'haya14busa/vim-edgemotion'})
+table.insert(pluginlist, {'rapan931/lasterisk.nvim'})
+table.insert(pluginlist, {'kevinhwang91/nvim-hlslens'})
+-- table.insert(pluginlist,{
+--     })
+-- table.insert(pluginlist,{
+--     })
+if (vim.g.vscode == nil) then
+    table.insert(pluginlist, {
+        'nvim-lualine/lualine.nvim',
+        version = "*"
+    })
+    table.insert(pluginlist, {'neoclide/coc.nvim'})
+    table.insert(pluginlist, {'yuki-yano/fzf-preview.vim'})
+end
+return pluginlist
