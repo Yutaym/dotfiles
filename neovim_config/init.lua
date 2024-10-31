cc6ae64bffb9a34fb7e19318bdfe7d3af3810eb8
YutaNaist <yuta.yamamoto@ms.naist.jp>
2024-10-31 10:14:02 +0900

    Refactor lazy.nvim setup and plugin list, improve cmpconfig.lua

--- neovim_config/init.lua
+++ neovim_config/init.lua
@@ -5,8 +5,11 @@ require("config.lazy")
 require("config.toggleterm")
 require("config.comment")
 require("config.plugins")
+
+if (vim.g.vscode == nil) then
 require("config.lspconfig")
 require("config.cmpconfig")
+end
 
 require("function.toggleMotion")
 
