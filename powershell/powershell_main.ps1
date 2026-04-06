75052a0dccb5992941c06db8a6a7271831429a54
YutaNaist <yuta.yamamoto@ms.naist.jp>
2026-04-06 13:01:00 +0900

    PowerShellスクリプトにプロファイル、エイリアス、補完、関数の読み込みを追加

--- powershell/powershell_main.ps1
+++ powershell/powershell_main.ps1
@@ -7,3 +7,29 @@ $condaPath = Join-Path $env:USERPROFILE "miniconda3\Scripts\conda.exe"
 If (Test-Path $condaPath) {
     (& $condaPath "shell.powershell" "hook") | Out-String | ? { $_ } | Invoke-Expression
 }
+
+$powershellProfile = Join-Path $PSScriptRoot "powershell_profile.ps1"
+$powershellAlias = Join-Path $PSScriptRoot "powershell_alias.ps1"
+$powershellComplete = Join-Path $PSScriptRoot "powershell_complete.ps1"
+$powershellFunction = Join-Path $PSScriptRoot "powershell_function.ps1"
+# $powershellProfile = Join-Path $env:USERPROFILE "dotfiles\powershell\powershell_profile.ps1"
+# $powershellAlias = Join-Path $env:USERPROFILE "dotfiles\powershell\powershell_alias.ps1"
+# $powershellComplete = Join-Path $env:USERPROFILE "dotfiles\powershell\powershell_complete.ps1"
+# $powershellFunction = Join-Path $env:USERPROFILE "dotfiles\powershell\powershell_function.ps1"
+if (Test-Path $powershellProfile) {
+    . $powershellProfile
+}
+if (Test-Path $powershellAlias) {
+    . $powershellAlias
+}
+if (Test-Path $powershellComplete) {
+    . $powershellComplete
+}
+if (Test-Path $powershellFunction) {
+    . $powershellFunction
+}
+
+# $powershellProfile = Join-Path $env:USERPROFILE "dotfiles\powershell\powershell_main.ps1"
+# if (Test-Path $powershellMain) {
+#     . $powershellMain
+# }
